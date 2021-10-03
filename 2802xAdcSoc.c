f669a0faf1f365a7240f4ff986090d2d16ba89ea
B. Atakan Arıkan <atakanarikan@outlook.com>
2021-10-03 18:41:27 +0300

    Update 2802xAdcSoc.c

--- 2802xAdcSoc.c
+++ 2802xAdcSoc.c
@@ -1,4 +1,4 @@
-*/ #include "DSP28x_Project.h"
+/* #include "DSP28x_Project.h"
 #include "f2802x_headers/include/F2802x_CpuTimers.h"
 #include "f2802x_common/include/F2802x_globalprototypes.h" */
 interrupt void cpu_timer1_isr(void);
